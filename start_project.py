a6bede2fb843c4ce7f1348d499b212ab6b544839
hanenia <142868438+hanenia@users.noreply.github.com>
2024-01-19 21:30:47 -0800

    Update start_project.py

--- start_project.py
+++ start_project.py
@@ -1,20 +1,74 @@
-""" Start a data analytics project """
+"""Project 2: Creating folders"""
 
 import pathlib
+import math
+import time
+import Hanna__utils
 
-def create_project_directory(directory_name: str): # add type
-    """
-    adding things
-    """
-    pathlib.Path(directory_name).mkdir(exist_ok=True)
-
-def main():
-    create_project_directory(directory_name='test') # name the parameter
-    create_project_directory(directory_name='patient') # name the parameter
+"""Creat folders for a given range of years."""
+def create_folders_for_range(Start, end):
     
+    for year in range (Start, end + 1 ):
+         folder_path = pathlib.Path(f'year_{year}')
+         folder_path.mkdir(exist_ok=True)
 
-if __name__ == '__main__':
+
+"""Create Prefixed folders"""
+def create_prefixed_folders(folders_list, prefix):
+     
+    for item in folders_list: 
+          folder_path = pathlib.Path(f'{prefix}{item}')
+          folder_path.mkdir(exist_ok=True)
+		  
+""" Create folders from list """
+def create_folders_from_list(folder_list):
+    for folder_name in folder_list:
+        path = pathlib.Path(folder_name)
+        path.mkdir(exist_ok=True)
+		  
+""" Create prefixed folders"""
+def create_prefixed_folders(folder_list, prefix):
+    
+    for folder_name in folder_list:
+        folder = pathlib.Path(prefix + folder_name)
+        folder.mkdir(parents=True, exist_ok=True)
+
+
+
+"""Creat folders periodically using a while loop"""
+def create_folders_periodically(duration):
+    
+     counter=0
+     while counter<5:
+          folder_path = pathlib.Path(f'Folder_{counter}')
+          folder_path.mkdir(exist_ok=True)
+          counter += 1
+          time.sleep(duration)
+
+""" Create folders periodically """		  
+def create_folders_periodically(duration):
+    
+    num_folders = 5 
+    folder_count = 1
+
+    while folder_count <= num_folders:
+        pathlib.Path("folder-" + str(folder_count)).mkdir(exist_ok=True)
+        folder_count += 1
+        time.sleep(duration)
+
+
+def main ():
+     """Main function to demostrate module is capabilities"""
+
+     create_folders_for_range(2010,2019)
+     folder_names = ['data-csv', 'data-excel,' 'data-json']
+     create_folders_for_list(folder_names)
+     prefix = 'data-'
+     create_prefixed_folders(folder_names, prefix)
+
+     create_folders_periodically(5)
+
+if __name__== '__main__':
     main()
-
-
+     
     
